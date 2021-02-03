dff9d0c8731f3394e37d201db46b6ff941842dc5
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-02-02 22:20:39 -0800

    Back to square one ... fun with advanced traits

--- src/api.rs
+++ src/api.rs
@@ -1,28 +1,43 @@
-
 #![allow(warnings)]
 struct FlareContext;
 
 impl FlareContext {
     fn textFile(&self, name: String) -> TextFileRDD {
-        unimplemented!()
+        TextFileRDD { name }
     }
 }
 
-trait RDDBase<T> {
+trait RDDBase {
+    type T;
 
+    fn map<F, U>(self, mapfn: F) -> MapRDD<F, Self>
+    where
+        Self: Sized,
+        F: FnMut(Self::T) -> U,
+    {
+        MapRDD {
+            mapfn,
+            source: self,
+        }
+    }
+}
+
+struct MapRDD<F, T> {
+    mapfn: F,
+    source: T,
 }
 
 struct TextFileRDD {
-
+    name: String,
 }
 
-impl RDDBase<String> for TextFileRDD {
-
+impl RDDBase for TextFileRDD {
+    type T = String;
 }
 
 #[test]
 fn test() {
-    let fc = FlareContext;
-    let tf = fc.textFile("./foo.csv".to_owned());
+    let fc = FlareContext {};
 
+    let tf = fc.textFile("./foo.csv".to_owned()).map(|e| e.len());
 }
