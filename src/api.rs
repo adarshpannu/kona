9c0eccc43689e25de1ec6ff012973023f0a2f41b
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-02-03 12:42:31 -0800

    Ok

--- src/api.rs
+++ src/api.rs
@@ -1,19 +1,36 @@
 #![allow(warnings)]
+
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+use io::BufReader;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
 struct FlareContext;
 
 impl FlareContext {
-    fn textFile(&self, name: String) -> TextFileRDD {
-        TextFileRDD { name }
+    fn textFile(&self, filename: String) -> TextFileRDD {
+        TextFileRDD::new(filename)
     }
 }
 
 trait RDDBase {
-    type T;
+    type Item;
+
+    fn next(&mut self) -> Option<Self::Item>;
 
     fn map<F, U>(self, mapfn: F) -> MapRDD<F, Self>
     where
         Self: Sized,
-        F: FnMut(Self::T) -> U,
+        F: FnMut(Self::Item) -> U,
     {
         MapRDD {
             mapfn,
@@ -22,22 +39,58 @@ trait RDDBase {
     }
 }
 
-struct MapRDD<F, T> {
+struct MapRDD<F, R> {
     mapfn: F,
-    source: T,
+    source: R,
+}
+
+impl<F, R, U> RDDBase for MapRDD<F, R>
+where
+    R: RDDBase,
+    F: FnMut(R::Item) -> U,
+{
+    type Item = U;
+
+    fn next(&mut self) -> Option<U> {
+        unimplemented!()
+    }
 }
 
 struct TextFileRDD {
-    name: String,
+    filename: String,
+    iter: io::Lines<io::BufReader<File>>,
 }
 
 impl RDDBase for TextFileRDD {
-    type T = String;
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(e) = self.iter.next() {
+            e.ok()
+        } else {
+            None
+        }
+    }
+}
+
+impl TextFileRDD {
+    fn new(filename: String) -> TextFileRDD {
+        let iter = read_lines(&filename).unwrap();
+        TextFileRDD { filename, iter }
+    }
 }
 
 #[test]
 fn test() {
     let fc = FlareContext {};
 
-    let tf = fc.textFile("./foo.csv".to_owned()).map(|e| e.len());
+    let mut rdd = fc
+        .textFile("/Users/adarshrp/.bashrc".to_owned())
+        .map(|e| e.len());
+
+    while let Some(line) = rdd.next() {
+        println!("-- {:?}", line);
+    }
 }
+
+fn foo(a: i32, b: i32) {}
