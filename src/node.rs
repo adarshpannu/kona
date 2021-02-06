b1766aa0d56060a90c00712763aa87fdd66c6884
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2021-02-05 23:32:33 -0800

    WIP

--- src/node.rs
+++ src/node.rs
@@ -1,10 +1,11 @@
 #![allow(warnings)]
 
-use std::cell::RefCell;
-use std::rc::Rc;
 use crate::expr;
+use std::cell::RefCell;
+use std::fmt;
+use std::rc::Rc;
 
-use super::expr::{*, Expr::*};
+use super::expr::{Expr::*, *};
 
 /***************************************************************************************************/
 trait Node {
@@ -22,24 +23,48 @@ trait Node {
     {
         FilterNode::new(self, expr)
     }
-
 }
 
 /***************************************************************************************************/
+#[derive(Debug)]
 struct CSVScanNode<'a> {
     filename: &'a str,
+    iter: io::Lines<io::BufReader<File>>,
 }
 
 impl<'a> Node for CSVScanNode<'a> {}
 
 impl<'a> CSVScanNode<'a> {
     fn new(filename: &str) -> CSVScanNode {
-        CSVScanNode { filename }
+        let iter = read_lines(&filename).unwrap();
+
+        CSVScanNode { filename, iter }
     }
 }
 
-/***************************************************************************************************/
+impl<'a> fmt::Display for CSVScanNode<'a> {
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "CSVScanNode({})", self.filename)
+    }
+}
 
+use fmt::Display;
+use io::BufReader;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+/***************************************************************************************************/
+#[derive(Debug)]
 struct SelectNode<T> {
     cols: Vec<usize>,
     child: T,
@@ -53,11 +78,21 @@ impl<T> SelectNode<T> {
 
 impl<T> Node for SelectNode<T> {}
 
-/***************************************************************************************************/
+impl<T> fmt::Display for SelectNode<T>
+where
+    T: Display,
+{
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "SelectNode({:?}, {})", &self.cols, &self.child)
+    }
+}
 
+/***************************************************************************************************/
+#[derive(Debug)]
 struct FilterNode<T> {
     child: T,
-    expr: Expr
+    expr: Expr,
 }
 
 impl<T> FilterNode<T> {
@@ -72,16 +107,30 @@ impl<T> FilterNode<T> {
 
 impl<T> Node for FilterNode<T> {}
 
+impl<T> fmt::Display for FilterNode<T>
+where
+    T: Display,
+{
+    // This trait requires `fmt` with this exact signature.
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "FilterNode({}, {})", &self.expr, &self.child)
+    }
+}
 
 /***************************************************************************************************/
 #[test]
 fn test() {
     let expr = RelExpr(
-        Box::new(CID(10) + CID(20)),
+        Box::new(CID(1)),
         RelOp::Gt,
-        Box::new(IntegerLiteral(30)),
+        Box::new(IntegerLiteral(20)),
     );
 
-    println!("{}", expr);
-    let node = CSVScanNode::new("c:/").select(vec![0, 1]).filter(expr);
+    let filename = "/Users/adarshrp/Projects/flare/src/data/emp.csv";
+    let node = CSVScanNode::new(filename)
+        .select(vec![0, 1])
+        .filter(expr)
+        .select(vec![0, 1]);
+
+    println!("{}", node);
 }
