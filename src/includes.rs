d3154b04f1e22b539b8d94d65feef1e09e64f2d1
Adarsh Pannu <13154869+adarshpannu@users.noreply.github.com>
2023-05-28 16:58:50 -0700

    Cleanup includes

--- src/includes.rs
+++ src/includes.rs
@@ -1,7 +1,16 @@
 // includes.rs
 pub use std::fs;
 
+pub use crate::env::Env;
+pub use arrow2::{
+    array::{Array, BooleanArray, PrimitiveArray},
+    chunk::Chunk,
+    compute::{arithmetics, boolean, comparison},
+    datatypes::{DataType, Field},
+};
 pub use log::{debug, error, info, warn};
+pub use serde::{Deserialize, Serialize};
+pub use std::mem::replace;
 
 //pub const DATADIR: &str = "/Users/adarshrp/Projects/flare/data";
 pub const DATADIR: &str = "/Users/adarshrp/Projects/tpch-data/sf0.01";
@@ -12,22 +21,13 @@ pub type ColId = usize;
 pub type QunId = usize;
 pub type QBId = usize;
 pub type PartitionId = usize;
-pub type RegisterId = usize;
 pub type StageId = usize;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug, PartialOrd, Ord, Serialize, Deserialize)]
 pub struct QunCol(pub QunId, pub ColId);
 
-pub type ColMap = std::collections::HashMap<ColId, ColId>;
-
-pub use serde::{Deserialize, Serialize};
-
-pub use crate::env::Env;
-
 #[derive(Debug, Clone, Copy, Serialize, Deserialize)]
 pub struct TextFilePartition(pub u64, pub u64);
-
-pub use std::mem::replace;
 
 pub fn enquote(s: &String) -> String {
     format!("\"{}\"", s)
@@ -73,13 +73,6 @@ pub fn list_files(dirname: &String) -> Result<Vec<String>, String> {
     }
     Ok(pathnames)
 }
-
-pub use arrow2::{
-    array::{Array, BooleanArray, PrimitiveArray},
-    chunk::Chunk,
-    compute::{arithmetics, boolean, comparison},
-    datatypes::{DataType, Field},
-};
 
 pub type ChunkBox = Chunk<Box<dyn Array>>;
 
